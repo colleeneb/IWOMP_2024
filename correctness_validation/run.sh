7db15b8dd06b9fffba17a042c63307671ec9dce0
Colleen Bertoni <bertoni@x4000c6s0b0n0.hostmgmt.cm.aurora.alcf.anl.gov>
2025-04-16 13:58:45 +0000

    updating OvO and OMPVV results

--- correctness_validation/run.sh
+++ correctness_validation/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+INTEL_FLAGS="-fiopenmp -fopenmp-targets=spir64 -fp-model=precise"
+
 rm -rf OvO
 git clone https://github.com/TApplencourt/OvO.git
 cd OvO
 
 export OVO_TIMEOUT=600s
 
-CXX='icpx' CXXFLAGS="-std=c++17 -fiopenmp -fopenmp-targets=spir64" FC="ifx" FFLAGS="-fiopenmp -fopenmp-targets=spir64" ./ovo.sh run test_src
+CXX='icpx' CXXFLAGS="-std=c++17 ${INTEL_FLAGS}" FC="ifx" FFLAGS="${INTEL_FLAGS}" ./ovo.sh run test_src
 
 ./ovo.sh report --summary test_result
  
@@ -17,17 +19,27 @@ git clone git@github.com:OpenMP-Validation-and-Verification/OpenMP_VV.git
 
 cd OpenMP_VV
 
-make OMP_VERSION=4.5 CC='icx -std=c99 -fiopenmp -fopenmp-targets=spir64' CXX='icpx -std=c++11 -fiopenmp -fopenmp-targets=spir64'  FC='ifx -fiopenmp -fopenmp-targets=spir64' VERBOSE_TESTS=1 VERBOSE=1 LOG=1 LOG_ALL=1 all
+make OMP_VERSION=4.5 CC='icx -std=c99 ${INTEL_FLAGS}' CXX='icpx -std=c++11 ${INTEL_FLAGS}'  FC='ifx ${INTEL_FLAGS}' VERBOSE_TESTS=1 VERBOSE=1 LOG=1 LOG_ALL=1 all
 make report_summary
 
 make tidy
 
-make OMP_VERSION=5.0 CC='icx -std=c99 -fiopenmp -fopenmp-targets=spir64' CXX='icpx -std=c++11 -fiopenmp -fopenmp-targets=spir64'  FC='ifx -fiopenmp -fopenmp-targets=spir64' VERBOSE_TESTS=1 VERBOSE=1 LOG=1 LOG_ALL=1 all
+make OMP_VERSION=5.0 CC='icx -std=c99 ${INTEL_FLAGS}' CXX='icpx -std=c++11 ${INTEL_FLAGS}'  FC='ifx ${INTEL_FLAGS}' VERBOSE_TESTS=1 VERBOSE=1 LOG=1 LOG_ALL=1 all
 make report_summary
 
 make tidy
 
-make OMP_VERSION=5.1 CC='icx -std=c99 -fiopenmp -fopenmp-targets=spir64' CXX='icpx -std=c++11 -fiopenmp -fopenmp-targets=spir64'  FC='ifx -fiopenmp -fopenmp-targets=spir64' VERBOSE_TESTS=1 VERBOSE=1 LOG=1 LOG_ALL=1 all
+make OMP_VERSION=5.1 CC='icx -std=c99 ${INTEL_FLAGS}' CXX='icpx -std=c++11 ${INTEL_FLAGS}'  FC='ifx ${INTEL_FLAGS}' VERBOSE_TESTS=1 VERBOSE=1 LOG=1 LOG_ALL=1 all
+make report_summary
+
+make tidy
+
+make OMP_VERSION=5.2 CC='icx -std=c99 ${INTEL_FLAGS}' CXX='icpx -std=c++11 ${INTEL_FLAGS}'  FC='ifx ${INTEL_FLAGS}' VERBOSE_TESTS=1 VERBOSE=1 LOG=1 LOG_ALL=1 all
+make report_summary
+
+make tidy
+
+make OMP_VERSION=6.0 CC='icx -std=c99 ${INTEL_FLAGS}' CXX='icpx -std=c++11 ${INTEL_FLAGS}'  FC='ifx ${INTEL_FLAGS}' VERBOSE_TESTS=1 VERBOSE=1 LOG=1 LOG_ALL=1 all
 make report_summary
 
 make tidy
