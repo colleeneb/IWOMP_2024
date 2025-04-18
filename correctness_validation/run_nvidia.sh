56766694c391c7706480edf4dde9e057cadb03d6
Colleen Bertoni <bertoni@anl.gov>
2025-04-18 08:18:00 -0500

    adding polaris outputs

--- correctness_validation/run_nvidia.sh
+++ correctness_validation/run_nvidia.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+# PrgEnv-nvhpc/8.5.0
 
-module load nvhpc/nvhpc/24.1 cuda/12.3.0 
+export NVIDIA_FLAGS="-mp=gpu -gpu=cc80"
 
-export NVIDIA_FLAGS="-mp=gpu -gpu=cc90"
-
-rm -rf OvO
-git clone https://github.com/TApplencourt/OvO.git
+#rm -rf OvO
+#git clone https://github.com/TApplencourt/OvO.git
 cd OvO
 
-export OVO_TIMEOUT=200s
+export OVO_TIMEOUT=60s
+rm -rf test_result
 
 CXX='nvc++' CXXFLAGS="-std=c++17 ${NVIDIA_FLAGS}" FC="nvfortran" FFLAGS="${NVIDIA_FLAGS}" ./ovo.sh run test_src
 
 ./ovo.sh report --summary test_result
  
-
-rm -rf OpenMP_VV
-git clone git@github.com:OpenMP-Validation-and-Verification/OpenMP_VV.git
+cd ..
+#rm -rf OpenMP_VV
+#git clone git@github.com:OpenMP-Validation-and-Verification/OpenMP_VV.git
 
 cd OpenMP_VV
+
+export OMPVV_NO_COMPILER_CHANGES=1
+make tidy
 
 make OMP_VERSION=4.5 CC='nvc  ${NVIDIA_FLAGS}' CXX='nvc++  ${NVIDIA_FLAGS}'  FC='nvfortran ${NVIDIA_FLAGS}' VERBOSE_TESTS=1 VERBOSE=1 LOG=1 LOG_ALL=1 all
 make report_summary
